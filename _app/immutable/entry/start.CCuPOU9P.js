35650eeb3ecbb6b169871adc64bb8e9c6343afef
christos-golsouzidis <christos.golsouzidis@gmail.com>
2026-03-04 19:44:10 +0100

    fix tech on mobile view

--- _app/immutable/entry/start.CCuPOU9P.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/DRWKUP3Y.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.CX5qR5n_.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DbDUHv31.js";export{o as load_css,r as start};
